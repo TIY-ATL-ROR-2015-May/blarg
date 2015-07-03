0caa4628563d7b73f3449879879aaf58f48299dc
Brit Butler <redline6561@gmail.com>
2015-07-03 11:08:48 -0400

    Add a file field on the form and tell strong params to allow it in the controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_devise_params, if: :devise_controller?
 
   rescue_from ActiveRecord::RecordNotFound do
     render json: { error: "No such item: #{request.path} " },
       status: :not_found
+  end
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) << :avatar
+    devise_parameter_sanitizer.for(:account_update) << :avatar
   end
 end
